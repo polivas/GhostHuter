068b03ee364b466746ea7a98b699900138fffab5
paulina <polivas312@ksu>
2022-02-21 17:05:20 -0600

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace SceenGame
+{
+    public static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            using (var game = new MainGame())
+                game.Run();
+        }
+    }
+}
